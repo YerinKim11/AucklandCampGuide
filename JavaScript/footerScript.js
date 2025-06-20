9bea44279e3f43a23eb6675edc7bfc43a1df655c
YerinKim <fnffnf95613@gmail.com>
2025-06-20 14:39:52 +1200

    Update navbar and layout for responsive design

--- JavaScript/footerScript.js
+++ JavaScript/footerScript.js
@@ -2,8 +2,6 @@
 document.addEventListener("DOMContentLoaded", function (){ 
   
     var foot = document.getElementById("myfooter");
-    //It helps the script dynamically find the correct path to menu.html or footer.html depending on where the current HTML file is located.
-    var pathPrefix = location.pathname.includes("pages") ? "../" : "";
     var xmlhttp = new XMLHttpRequest();
     xmlhttp.open("GET","footer.html",false);
     xmlhttp.send();
